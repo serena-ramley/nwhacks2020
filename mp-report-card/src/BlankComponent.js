1471606877e39913e7e45b27e7469b30418ba62e
serena-ramley <sramley@gmail.com>
2020-01-11 16:24:29 -0800

    Set up template layout

---
+++ mp-report-card/src/BlankComponent.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class BlankComponent extends Component {
+
+    render() {
+      return (
+        <div>
+            <p> Hello I am an empty component</p>
+        </div>
+    )
+    }
+  }
+
+export default BlankComponent;
