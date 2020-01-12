f5bb17116f9b62611bc6cefda4a9c02db04fefac
serena-ramley <sramley@gmail.com>
2020-01-11 16:56:30 -0800

    Added states

--- mp-report-card/src/App.js
+++ mp-report-card/src/App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import BlankComponent from './BlankComponent';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <BlankComponent />
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+        data: [],
+        searchField: '',
+    }
+  }
+
+  onSearchChange = (event) => {
+    this.setState( {searchField: event.target.value})
+  }
+
+  render () {
+      return (
+      <div className="App">
+        <header className="App-header">
+          <h1> Website Title</h1>
+          <BlankComponent />
+          <BlankComponent />
+        </header>
+      </div>
+      )
+  }
 }
 
 export default App;
