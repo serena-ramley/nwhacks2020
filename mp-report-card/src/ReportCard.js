13e4df0ecdf18c10845f2f927c9a90342f70cca4
serena-ramley <sramley@gmail.com>
2020-01-11 16:59:13 -0800

    Added empty report card

---
+++ mp-report-card/src/ReportCard.js
@@ -0,0 +1,15 @@
+import React, {Component} from 'react';
+
+
+class ReportCard extends Component {
+
+    render() {
+      return (
+        <div>
+            <p> Hello I am an empty component</p>
+        </div>
+    )
+    }
+  }
+
+export default ReportCard;
