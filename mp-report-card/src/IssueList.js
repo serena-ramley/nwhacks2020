f63b6f694426073823394bfa4ca9a05116af8730
serena-ramley <sramley@gmail.com>
2021-01-14 12:33:09 -0800

    Update IssueList.js

--- mp-report-card/src/IssueList.js
+++ mp-report-card/src/IssueList.js
@@ -32,25 +32,4 @@ class ReportCard extends Component {
 export default ReportCard;
 
 
-const issues = ["Abortion", "Euthanasia", "Gay Marriage", "LGBT Adoption Rights", "Gender Workplace Diversity", 
-"Gender Identity", "Women in Combat", "Niqab", "Death Penalty", "Transgender Athletes", 
-"Environmental Regulation", "Logging", "Animal Testing", "Plastic Product Ban", "Fracking", 
-"Northern Gateway Pipeline", "Renewable Energy", "Genetically Modified Foods", "Keystone Pipeline", 
-"Corporate Subsidies", "Mental Health", "Prescription Drugs", "Healthcare Funding", "Healthcare", 
-"Dental Coverage", "Single-Payer Healthcare", "Healthcare Privatization", "Marijuana", "Safe Haven", 
-"Welfare Drug Testing", "Taxes", "Minimum Wage", "Corporate Tax", "Welfare", "Universal Child Care", 
-"Old Age Pensions", "Universal Basic Income", "Pension Tax", "Labor Unions", "Income Splitting", 
-"Government Spending", "Free Trade", "Government Pensions", "Domestic Jobs", "Bonus Cap", 
-"Employment Insurance", "Economic Stimulus", "Pension Premiums", "Offshore Banking", "NAFTA", 
-"Tech Monopolies", "Tariffs", "Trans-Pacific Partnership", "Bitcoin", "Armed Teachers", 
-"Government Surveillance", "Gun Control", "Net Neutrality", "Quebec Sovereignty", "Native Funding", 
-"Drug Policy", "Whistleblower Protection", "Social Media Regulation", "National Daycare", "Commuter Rail", 
-"Worker Strikes", "Energy Sector", "Flag Burning", "Senate", "CBC Funding", "Long Form Cencus", "Copyright", 
-"CSA Funding", "University Tution", "Postsecondary Education", "Charter Schools", "Deporting Criminal Immigrants", 
-"Citizenship Test", "Immigration Ban", "Immigrant Assimilation", "Skilled Immigrants", "Dual Citizenship", 
-"Temporary Foreign Workers", "Sanctuary Cities", "Political Pary Subsidies", "Campaign Finance", 
-"Prime Minister Term Limits", "Criminal Politicians", "Right of Foreigners to Vote", "Candidate Transparency", 
-"Minimum Voting Age", "Electoral Reform", "Private Prisons", "Parole Hearings", "Drug Trafficking Penalties", 
-"Criminal Voting Rights", "Prison Overcrowding", "Mandatory Military Service", "United Nations", 
-"Foreign Elections", "Syrian Refugees", "Bill C-51", "Israel Boycott", "Foreign Aid", 
-"Military Spending", "Nuclear Energy", "Public Transportation", "Commercial Drones"];
+const issues = ["Education", "Employment", "Transportation", "Energy", "Environment", "Health Care"];
